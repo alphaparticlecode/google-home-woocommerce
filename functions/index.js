d1cb56dac42700df74d7f5d0c0bc89c8817c8217
Keanan Koppenhaver <keanan@howtogeek.com>
2018-04-22 17:26:07 -0500

    First working version of sale endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,24 @@
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
 'use strict';
-
-const {dialogflow} = require('actions-on-google');
+ 
 const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
+  function sale_items(agent) {
+    agent.add(`Sorry, there is nothing on sale today. Check back tomorrow!`);
+  }
 
-const app = dialogflow({debug: true});
-
-app.intent('sale', (conv, {color, number}) => {
-  conv.close(`There are no sales today. Check back again tomorrow!`);
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('sale_items', sale_items);
+  agent.handleRequest(intentMap);
 });
-
-exports.acmeWidgetCatalog = functions.https.onRequest(app);
