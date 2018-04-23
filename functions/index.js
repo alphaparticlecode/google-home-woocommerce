442693e6c6f4b163afd4909efa2268d6be78002d
Keanan Koppenhaver <keanan@howtogeek.com>
2018-04-22 23:16:37 -0500

    Initial API call checkpoint

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
+const rpn = require('request-promise-native');
  
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
  
@@ -14,7 +15,27 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
  
   function sale_items(agent) {
-    agent.add(`Sorry, there is nothing on sale today. Check back tomorrow!`);
+
+    // WooCommerce Consumer Key, Consumer Secret stored in Firebase environment variables
+    // Base64 encoded and separated by :
+    rpn.get('https://acmewidget.alphaparticle.com/wp-json/wc/v2/products?filter[tag]=sale&filter[limit]=3')
+    .auth(functions.config().acmewidget.key, functions.config().acmewidget.secret, false)
+    .then(jsonBody => {
+    	console.log(jsonBody);
+    	var body = JSON.parse(jsonBody);
+
+    	console.log(body);
+    	agent.add('API resolved!');
+    	return Promise.resolve( agent ); 
+    })
+    .catch(jsonBody => {
+    	console.log(jsonBody);
+    	var body = JSON.parse(jsonBody);
+
+    	console.log(body);
+    	agent.add('API did not resolve!');
+    	return Promise.reject( agent ); 
+    });
   }
 
   function todays_hours(agent) {
